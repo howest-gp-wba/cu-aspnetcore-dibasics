f3a68f32303e3141dceafdd18421fc1cc1c442d2
Bart Soete <bart.soete@howest.be>
2022-07-03 19:22:51 +0200

    Migrate to 6.0

--- src/CoreCourse.DIBasics.Web/Program.cs
+++ src/CoreCourse.DIBasics.Web/Program.cs
@@ -1,26 +1,59 @@
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using CoreCourse.DIBasics.Core.CoffeeSystem;
+using CoreCourse.DIBasics.Web.CoffeeWeb;
 
-namespace CoreCourse.DIBasics.Web
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+int CoffeeSettings = builder.Configuration.GetSection("CoffeeSettings").Get<int>();
+
+switch (CoffeeSettings)
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
+    case 1: //1 == The app needs coffee with cream and sugar
+        builder.Services.AddTransient<ICreamer, Cream>();
+        builder.Services.AddTransient<ISweetener, Sugar>();
+        break;
+    case 2: //2 == The app needs coffee with milk and honey
+        builder.Services.AddTransient<ICreamer, Milk>();
+        builder.Services.AddTransient<ISweetener, Honey>();
+        break;
+    case 3: //3 == The app needs coffee with liquor
+        builder.Services.AddTransient<ICreamer, Whiskey>();
+        builder.Services.AddTransient<ISweetener, Sugar>();
+        break;
+    case 4: //4 == The app needs "Random" implementations of creamers and sweeteners
+        builder.Services.AddTransient<ICreamer, RandomCreamer>();
+        builder.Services.AddTransient<ISweetener, RandomSweetener>();
+        break;
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
-    }
 }
+
+builder.Services.AddTransient<ICoffeeCup, CoffeeCup>();
+
+var random = new Random();
+builder.Services.AddSingleton<Random>(random);
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
